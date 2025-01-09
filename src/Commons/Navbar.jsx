3164d0481baa4c0fe8576f0ed0467a966beb8fb3
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-01-09 01:17:26 -0500

    Bug Fix: Updated home redirect in navbar

--- src/Commons/Navbar.jsx
+++ src/Commons/Navbar.jsx
@@ -25,10 +25,8 @@ const Navbar = ({ onLogOut, isLogin, toggleState, userId }) => {
       <div className="container mx-auto px-4 md:px-8 lg:px-16 flex justify-between items-center py-4">
         {/* Logo */}
         <div className="flex flex-col items-center">
-          <Link to={handleHomeDirect}>
-            <img src={wealthWise} alt="WealthWise Logo" className="w-12 h-12" />
-          </Link>
-          <div className="text-xl font-bold text-white mt-2">
+            <img onClick={handleHomeDirect} src={wealthWise} alt="WealthWise Logo" className="w-12 h-12" />
+          <div onClick={handleHomeDirect} className="text-xl font-bold text-white mt-2">
             WealthWise
           </div>
         </div>
